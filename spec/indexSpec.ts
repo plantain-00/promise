a0aa54a9a0c09602fe374bf6c05b245eb2a10114
york yao <yaoao12306@outlook.com>
2018-01-20 10:56:03 +0800

    refactor: use tslint-config-standard as lint rule

--- spec/indexSpec.ts
+++ spec/indexSpec.ts
@@ -1,194 +1,194 @@
-import Promise from "../dist/nodejs/index";
+import Promise from '../dist/nodejs/index'
 
-it("resolve then", () => {
-    new Promise<number>((resolve, reject) => {
-        resolve(1);
-    }).then(value => {
-        expect(value).toEqual(1);
-    });
+it('resolve then', () => {
+  new Promise<number>((resolve, reject) => {
+    resolve(1)
+  }).then(value => {
+    expect(value).toEqual(1)
+  })
 
-    new Promise<number>((resolve, reject) => {
-        resolve(2);
-    }).then(value => {
-        expect(value).toEqual(2);
-    }, reason => {
-        fail();
-    });
-});
+  new Promise<number>((resolve, reject) => {
+    resolve(2)
+  }).then(value => {
+    expect(value).toEqual(2)
+  }, reason => {
+    fail()
+  })
+})
 
-it("resolve resolve then", () => {
-    new Promise<number>((resolve, reject) => {
-        resolve(1.1);
-        resolve(1.2);
-    }).then(value => {
-        expect(value).toEqual(1.1);
-    });
-});
+it('resolve resolve then', () => {
+  new Promise<number>((resolve, reject) => {
+    resolve(1.1)
+    resolve(1.2)
+  }).then(value => {
+    expect(value).toEqual(1.1)
+  })
+})
 
-it("reject then", () => {
-    new Promise<number>((resolve, reject) => {
-        reject(3);
-    }).then(value => {
-        fail();
-    }, reason => {
-        expect(reason).toEqual(3);
-    });
-});
+it('reject then', () => {
+  new Promise<number>((resolve, reject) => {
+    reject(3)
+  }).then(value => {
+    fail()
+  }, reason => {
+    expect(reason).toEqual(3)
+  })
+})
 
-it("reject then catch", () => {
-    new Promise<number>((resolve, reject) => {
-        reject(4);
-    }).then(value => {
-        fail();
-    }, reason => {
-        expect(reason).toEqual(4);
-    }).catch(reason => {
-        fail();
-    });
+it('reject then catch', () => {
+  new Promise<number>((resolve, reject) => {
+    reject(4)
+  }).then(value => {
+    fail()
+  }, reason => {
+    expect(reason).toEqual(4)
+  }).catch(reason => {
+    fail()
+  })
 
-    new Promise<number>((resolve, reject) => {
-        reject(5);
-    }).then(value => {
-        fail();
-    }).catch(reason => {
-        expect(reason).toEqual(5);
-    });
+  new Promise<number>((resolve, reject) => {
+    reject(5)
+  }).then(value => {
+    fail()
+  }).catch(reason => {
+    expect(reason).toEqual(5)
+  })
 
-    new Promise<number>((resolve, reject) => {
-        reject(5);
-    }).then(value => {
-        fail();
-    }).then(value => {
-        fail();
-    }).catch(reason => {
-        expect(reason).toEqual(5);
-    });
-});
+  new Promise<number>((resolve, reject) => {
+    reject(5)
+  }).then(value => {
+    fail()
+  }).then(value => {
+    fail()
+  }).catch(reason => {
+    expect(reason).toEqual(5)
+  })
+})
 
-it("throw then catch", () => {
-    new Promise<number>((resolve, reject) => {
-        throw 6;
-    }).then(value => {
-        fail();
-    }).catch(reason => {
-        expect(reason).toEqual(6);
-    });
-});
+it('throw then catch', () => {
+  new Promise<number>((resolve, reject) => {
+    throw 6
+  }).then(value => {
+    fail()
+  }).catch(reason => {
+    expect(reason).toEqual(6)
+  })
+})
 
-it("reject in then", () => {
-    new Promise<number>((resolve, reject) => {
-        resolve(7);
-    }).then(value => {
-        expect(value).toEqual(7);
-        return Promise.reject(value);
-    }).catch(reason => {
-        expect(reason).toEqual(7);
-    });
-});
+it('reject in then', () => {
+  new Promise<number>((resolve, reject) => {
+    resolve(7)
+  }).then(value => {
+    expect(value).toEqual(7)
+    return Promise.reject(value)
+  }).catch(reason => {
+    expect(reason).toEqual(7)
+  })
+})
 
-it("throw in then", () => {
-    new Promise<number>((resolve, reject) => {
-        resolve(8);
-    }).then(value => {
-        expect(value).toEqual(8);
-        throw value;
-    }).catch(reason => {
-        expect(reason).toEqual(8);
-    });
-});
+it('throw in then', () => {
+  new Promise<number>((resolve, reject) => {
+    resolve(8)
+  }).then(value => {
+    expect(value).toEqual(8)
+    throw value
+  }).catch(reason => {
+    expect(reason).toEqual(8)
+  })
+})
 
-it("return in then", () => {
-    new Promise<number>((resolve, reject) => {
-        resolve(9);
-    }).then(value => {
-        expect(value).toEqual(9);
-        return value;
-    }).then(value => {
-        expect(value).toEqual(9);
-    });
+it('return in then', () => {
+  new Promise<number>((resolve, reject) => {
+    resolve(9)
+  }).then(value => {
+    expect(value).toEqual(9)
+    return value
+  }).then(value => {
+    expect(value).toEqual(9)
+  })
 
-    new Promise<number>((resolve, reject) => {
-        resolve(17);
-    }).then(value => {
-        expect(value).toEqual(17);
-        return value + " string";
-    }).then(value => {
-        expect(value).toEqual("17 string");
-    });
-});
+  new Promise<number>((resolve, reject) => {
+    resolve(17)
+  }).then(value => {
+    expect(value).toEqual(17)
+    return value + ' string'
+  }).then(value => {
+    expect(value).toEqual('17 string')
+  })
+})
 
-it("return promise in then", () => {
-    new Promise<number>((resolve, reject) => {
-        resolve(10);
-    }).then(value => {
-        expect(value).toEqual(10);
-        return Promise.resolve(value);
-    }).then(value => {
-        expect(value).toEqual(10);
-    });
-});
+it('return promise in then', () => {
+  new Promise<number>((resolve, reject) => {
+    resolve(10)
+  }).then(value => {
+    expect(value).toEqual(10)
+    return Promise.resolve(value)
+  }).then(value => {
+    expect(value).toEqual(10)
+  })
+})
 
-it("promise.all resolve", () => {
-    Promise.all([
-        Promise.resolve(11),
-        Promise.resolve(12),
-    ]).then(values => {
-        expect(values).toEqual([11, 12]);
-    });
+it('promise.all resolve', () => {
+  Promise.all([
+    Promise.resolve(11),
+    Promise.resolve(12)
+  ]).then(values => {
+    expect(values).toEqual([11, 12])
+  })
 
-    Promise.all([
-        new Promise(resolve => {
-            setTimeout(() => {
-                resolve(15);
-            }, 10);
-        }),
-        new Promise(resolve => {
-            setTimeout(() => {
-                resolve(16);
-            }, 10);
-        }),
-    ]).then(values => {
-        expect(values).toEqual([15, 16]);
-    });
-});
+  Promise.all([
+    new Promise(resolve => {
+      setTimeout(() => {
+        resolve(15)
+      }, 10)
+    }),
+    new Promise(resolve => {
+      setTimeout(() => {
+        resolve(16)
+      }, 10)
+    })
+  ]).then(values => {
+    expect(values).toEqual([15, 16])
+  })
+})
 
-it("promise.all reject", () => {
-    Promise.all([
-        Promise.resolve(13),
-        Promise.reject(14),
-    ]).then(values => {
-        fail();
-    }, reason => {
-        expect(reason).toEqual(14);
-    });
-});
+it('promise.all reject', () => {
+  Promise.all([
+    Promise.resolve(13),
+    Promise.reject(14)
+  ]).then(values => {
+    fail()
+  }, reason => {
+    expect(reason).toEqual(14)
+  })
+})
 
-it("reject catch", () => {
-    new Promise<number>((resolve, reject) => {
-        reject(18);
-    }).catch(reason => {
-        expect(reason).toEqual(18);
-    });
+it('reject catch', () => {
+  new Promise<number>((resolve, reject) => {
+    reject(18)
+  }).catch(reason => {
+    expect(reason).toEqual(18)
+  })
 
-    new Promise<number>((resolve, reject) => {
-        setTimeout(() => {
-            reject(19);
-        }, 10);
-    }).catch(reason => {
-        expect(reason).toEqual(19);
-    });
-});
+  new Promise<number>((resolve, reject) => {
+    setTimeout(() => {
+      reject(19)
+    }, 10)
+  }).catch(reason => {
+    expect(reason).toEqual(19)
+  })
+})
 
-it("resolve promise then", () => {
-    new Promise<number>((resolve, reject) => {
-        resolve(Promise.resolve(20));
-    }).then(value => {
-        expect(value).toEqual(20);
-    });
+it('resolve promise then', () => {
+  new Promise<number>((resolve, reject) => {
+    resolve(Promise.resolve(20))
+  }).then(value => {
+    expect(value).toEqual(20)
+  })
 
-    new Promise<number>((resolve, reject) => {
-        resolve(Promise.resolve(Promise.resolve(21)));
-    }).then(value => {
-        expect(value).toEqual(21);
-    });
-});
+  new Promise<number>((resolve, reject) => {
+    resolve(Promise.resolve(Promise.resolve(21)))
+  }).then(value => {
+    expect(value).toEqual(21)
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -2,197 +2,197 @@
  * @public
  */
 export default class Promise<T> {
-    public static all<T>(promises: Promise<T>[]): Promise<T[]> {
-        return new Promise<T[]>((resolve, reject) => {
-            const values: T[] = new Array(promises.length);
-            let resolvedPromiseCount = 0;
-            let rejected = false;
-            for (let i = 0; i < promises.length; i++) {
-                promises[i].then(value => {
-                    if (!rejected) {
-                        resolvedPromiseCount++;
-                        values[i] = value;
-                        if (resolvedPromiseCount === promises.length) {
-                            resolve(values);
-                        }
-                    }
-                }, reason => {
-                    if (!rejected) {
-                        rejected = true;
-                        reject(reason);
-                    }
-                });
+  private state = State.pending
+  private onFilfilled: (value: T) => void
+  private onRejected: (reason: any) => void
+  private onCatched: (reason: any) => void
+  private value: T
+  private reason: any
+
+  constructor (executor: (resolve: (value: T | Promise<T>) => void, reject: (reason: any) => void) => void) {
+    try {
+      executor(value => {
+        if (this.state === State.pending) {
+          this.state = State.fulfilled
+          resolvePromise(value, (newValue, reason) => {
+            if (newValue !== undefined) {
+              this.value = newValue
+              if (this.onFilfilled) {
+                this.onFilfilled(this.value)
+              }
+            } else {
+              this.reject(reason)
             }
-        });
+          })
+        }
+      }, reason => {
+        if (this.state === State.pending) {
+          this.reject(reason)
+        }
+      })
+    } catch (reason) {
+      if (this.state === State.pending) {
+        this.reject(reason)
+      }
     }
+  }
 
-    public static race<T>(promises: Promise<T>[]): Promise<T> {
-        return new Promise<T>((resolve, reject) => {
-            let pending = true;
-            for (const promise of promises) {
-                promise.then(value => {
-                    if (pending) {
-                        pending = false;
-                        resolve(value);
-                    }
-                }, reason => {
-                    if (pending) {
-                        pending = false;
-                        reject(reason);
-                    }
-                });
+  public static all<T> (promises: Promise<T>[]): Promise<T[]> {
+    return new Promise<T[]>((resolve, reject) => {
+      const values: T[] = new Array(promises.length)
+      let resolvedPromiseCount = 0
+      let rejected = false
+      for (let i = 0; i < promises.length; i++) {
+        promises[i].then(value => {
+          if (!rejected) {
+            resolvedPromiseCount++
+            values[i] = value
+            if (resolvedPromiseCount === promises.length) {
+              resolve(values)
             }
-        });
-    }
+          }
+        }, reason => {
+          if (!rejected) {
+            rejected = true
+            reject(reason)
+          }
+        })
+      }
+    })
+  }
 
-    public static reject<T>(reason: any): Promise<T> {
-        return new Promise<T>((resolve, reject) => {
-            reject(reason);
-        });
-    }
-    public static resolve<T>(value: T | Promise<T>): Promise<T> {
-        return new Promise<T>((resolve, reject) => {
-            resolve(value);
-        });
-    }
+  public static race<T> (promises: Promise<T>[]): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      let pending = true
+      for (const promise of promises) {
+        promise.then(value => {
+          if (pending) {
+            pending = false
+            resolve(value)
+          }
+        }, reason => {
+          if (pending) {
+            pending = false
+            reject(reason)
+          }
+        })
+      }
+    })
+  }
 
-    private state = State.pending;
-    private onFilfilled: (value: T) => void;
-    private onRejected: (reason: any) => void;
-    private onCatched: (reason: any) => void;
-    private value: T;
-    private reason: any;
+  public static reject<T> (reason: any): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      reject(reason)
+    })
+  }
+  public static resolve<T> (value: T | Promise<T>): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      resolve(value)
+    })
+  }
 
-    constructor(executor: (resolve: (value: T | Promise<T>) => void, reject: (reason: any) => void) => void) {
+  public then<TResult1, TResult2> (onFulfilled: (value: T) => never | TResult1 | Promise<TResult1>, onRejected?: (reason: any) => never | TResult2 | Promise<TResult2>): Promise<void | TResult1 | TResult2> {
+    const promise = new Promise<void | TResult1 | TResult2>((resolve, reject) => {
+      this.onFilfilled = value => {
         try {
-            executor(value => {
-                if (this.state === State.pending) {
-                    this.state = State.fulfilled;
-                    resolvePromise(value, (newValue, reason) => {
-                        if (newValue !== undefined) {
-                            this.value = newValue;
-                            if (this.onFilfilled) {
-                                this.onFilfilled(this.value);
-                            }
-                        } else {
-                            this.reject(reason);
-                        }
-                    });
-                }
-            }, reason => {
-                if (this.state === State.pending) {
-                    this.reject(reason);
-                }
-            });
+          const newResult = onFulfilled(value)
+          resolvePromise(newResult, (newValue, newReason) => {
+            if (newValue !== undefined) {
+              resolve(newValue)
+            } else {
+              reject(newReason)
+            }
+          })
         } catch (reason) {
-            if (this.state === State.pending) {
-                this.reject(reason);
-            }
+          reject(reason)
         }
-    }
-
-    public then<TResult1, TResult2>(onFulfilled: (value: T) => never | TResult1 | Promise<TResult1>, onRejected?: (reason: any) => never | TResult2 | Promise<TResult2>): Promise<void | TResult1 | TResult2> {
-        const promise = new Promise<void | TResult1 | TResult2>((resolve, reject) => {
-            this.onFilfilled = value => {
-                try {
-                    const newResult = onFulfilled(value);
-                    resolvePromise(newResult, (newValue, newReason) => {
-                        if (newValue !== undefined) {
-                            resolve(newValue);
-                        } else {
-                            reject(newReason);
-                        }
-                    });
-                } catch (reason) {
-                    reject(reason);
-                }
-            };
-            if (this.state === State.fulfilled) {
-                this.onFilfilled(this.value);
-            }
-            if (onRejected) {
-                this.onRejected = reason => {
-                    try {
-                        const newResult = onRejected(reason);
-                        resolvePromise(newResult, (newValue, newReason) => {
-                            if (newValue !== undefined) {
-                                resolve(newValue);
-                            } else {
-                                reject(newReason);
-                            }
-                        });
-                    } catch (newReason) {
-                        reject(newReason);
-                    }
-                };
-                if (this.state === State.rejected) {
-                    this.onRejected(this.reason);
-                }
-            } else {
-                setTimeout(() => {
-                    if (promise.onCatched) {
-                        this.onCatched = promise.onCatched;
-                        if (this.state === State.rejected) {
-                            this.onCatched(this.reason);
-                        }
-                    }
-                }, 0);
-            }
-        });
-        return promise;
-    }
-
-    public catch<TResult>(onRejected: (reason: any) => never | TResult | Promise<TResult>): Promise<void | TResult> {
-        return new Promise<void | TResult>((resolve, reject) => {
-            this.onCatched = reason => {
-                try {
-                    const newResult = onRejected(reason);
-                    resolvePromise(newResult, (newValue, newReason) => {
-                        if (newValue !== undefined) {
-                            resolve(newValue);
-                        } else {
-                            reject(newReason);
-                        }
-                    });
-                } catch (newReason) {
-                    reject(newReason);
-                }
-            };
-            if (this.state === State.rejected && this.reason !== undefined) {
-                this.onCatched(this.reason);
-            }
-        });
-    }
-
-    private reject(reason: any) {
-        if (reason) {
-            this.state = State.rejected;
-            this.reason = reason;
-            if (this.onRejected) {
-                this.onRejected(this.reason);
-            } else {
-                if (this.onCatched) {
-                    this.onCatched(this.reason);
-                }
-            }
+      }
+      if (this.state === State.fulfilled) {
+        this.onFilfilled(this.value)
+      }
+      if (onRejected) {
+        this.onRejected = reason => {
+          try {
+            const newResult = onRejected(reason)
+            resolvePromise(newResult, (newValue, newReason) => {
+              if (newValue !== undefined) {
+                resolve(newValue)
+              } else {
+                reject(newReason)
+              }
+            })
+          } catch (newReason) {
+            reject(newReason)
+          }
         }
+        if (this.state === State.rejected) {
+          this.onRejected(this.reason)
+        }
+      } else {
+        setTimeout(() => {
+          if (promise.onCatched) {
+            this.onCatched = promise.onCatched
+            if (this.state === State.rejected) {
+              this.onCatched(this.reason)
+            }
+          }
+        }, 0)
+      }
+    })
+    return promise
+  }
+
+  public catch<TResult> (onRejected: (reason: any) => never | TResult | Promise<TResult>): Promise<void | TResult> {
+    return new Promise<void | TResult>((resolve, reject) => {
+      this.onCatched = reason => {
+        try {
+          const newResult = onRejected(reason)
+          resolvePromise(newResult, (newValue, newReason) => {
+            if (newValue !== undefined) {
+              resolve(newValue)
+            } else {
+              reject(newReason)
+            }
+          })
+        } catch (newReason) {
+          reject(newReason)
+        }
+      }
+      if (this.state === State.rejected && this.reason !== undefined) {
+        this.onCatched(this.reason)
+      }
+    })
+  }
+
+  private reject (reason: any) {
+    if (reason) {
+      this.state = State.rejected
+      this.reason = reason
+      if (this.onRejected) {
+        this.onRejected(this.reason)
+      } else {
+        if (this.onCatched) {
+          this.onCatched(this.reason)
+        }
+      }
     }
+  }
 }
 
-function resolvePromise<T>(promise: T | Promise<T>, next: (value: T | undefined, reason: any) => void) {
-    if (promise instanceof Promise) {
-        promise.then(value => {
-            resolvePromise(value, next);
-        }, reason => {
-            next(undefined, reason);
-        });
-    } else {
-        next(promise, undefined);
-    }
+function resolvePromise<T> (promise: T | Promise<T>, next: (value: T | undefined, reason: any) => void) {
+  if (promise instanceof Promise) {
+    promise.then(value => {
+      resolvePromise(value, next)
+    }, reason => {
+      next(undefined, reason)
+    })
+  } else {
+    next(promise, undefined)
+  }
 }
 
 const enum State {
     pending,
     fulfilled,
-    rejected,
+    rejected
 }
